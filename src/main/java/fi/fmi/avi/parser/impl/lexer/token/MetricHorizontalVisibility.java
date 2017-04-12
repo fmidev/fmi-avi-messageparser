11c82a843e43899b9c1240f03307b2be1be7cbed
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-04-12 09:49:43 +0300

    The directions are now enumerated, with additional NDV value.

--- src/main/java/fi/fmi/avi/parser/impl/lexer/token/MetricHorizontalVisibility.java
+++ src/main/java/fi/fmi/avi/parser/impl/lexer/token/MetricHorizontalVisibility.java
@@ -9,6 +9,7 @@ import static fi.fmi.avi.parser.Lexeme.ParsedValueName.VALUE;
 import java.util.regex.Matcher;
 
 import fi.fmi.avi.parser.Lexeme;
+import fi.fmi.avi.parser.Lexeme.Status;
 import fi.fmi.avi.parser.ParsingHints;
 import fi.fmi.avi.parser.impl.lexer.RecognizingAviMessageTokenLexer;
 import fi.fmi.avi.parser.impl.lexer.RegexMatchingLexemeVisitor;
@@ -17,9 +18,43 @@ import fi.fmi.avi.parser.impl.lexer.RegexMatchingLexemeVisitor;
  * Created by rinne on 10/02/17.
  */
 public class MetricHorizontalVisibility extends RegexMatchingLexemeVisitor {
+	
+	public enum DirectionValue {
+		NORTH("N", 0),
+		SOUTH("S", 180),
+		EAST("E", 90),
+		WEST("W", 270),
+		NORTH_EAST("NE", 45),
+		NORTH_WEST("NW", 315),
+		SOUTH_EAST("SE", 135),
+		SOUTH_WEST("SW", 225),
+        NO_DIRECTIONAL_VARIATION("NDV",-1);
 
+        private String code;
+        private int deg;
+
+        DirectionValue(final String code, final int deg) {
+            this.code = code;
+            this.deg = deg;
+        }
+
+        public static DirectionValue forCode(final String code) {
+            for (DirectionValue w : values()) {
+                if (w.code.equals(code)) {
+                    return w;
+                }
+            }
+            return null;
+        }
+        
+        public int inDegrees() {
+        	return this.deg;
+        }
+
+    }
+	
     public MetricHorizontalVisibility(final Priority prio) {
-        super("^([0-9]{4})([A-Z]{1,2})?$", prio);
+        super("^([0-9]{4})([A-Z]{1,2}|NDV)?$", prio);
     }
 
     @Override
@@ -27,15 +62,24 @@ public class MetricHorizontalVisibility extends RegexMatchingLexemeVisitor {
         int visibility = Integer.parseInt(match.group(1));
         String direction = match.group(2);
         token.setParsedValue(UNIT, "m");
-        if (direction != null) {
-            token.setParsedValue(DIRECTION, direction);
-        }
+        
         if (visibility == 9999) {
-            token.setParsedValue(VALUE, 10000);
+            token.setParsedValue(VALUE, 10000d);
             token.setParsedValue(RELATIONAL_OPERATOR, RecognizingAviMessageTokenLexer.RelationalOperator.MORE_THAN);
         } else if (visibility == 0) {
-            token.setParsedValue(VALUE, 50);
+            token.setParsedValue(VALUE, 50d);
             token.setParsedValue(RELATIONAL_OPERATOR, RecognizingAviMessageTokenLexer.RelationalOperator.LESS_THAN);
+        } else {
+        	 token.setParsedValue(VALUE, new Double(visibility));
+        }
+        if (direction != null) {
+        	DirectionValue dv = DirectionValue.forCode(direction);
+        	if (dv != null) {
+        		token.setParsedValue(DIRECTION, dv);
+        	} else {
+        		token.identify(HORIZONTAL_VISIBILITY, Status.SYNTAX_ERROR, "Invalid visibility direction value '" + direction + "'");
+        		return;
+        	}
         }
         token.identify(HORIZONTAL_VISIBILITY);
     }
