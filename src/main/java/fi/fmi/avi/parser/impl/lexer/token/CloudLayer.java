b53e04db98b72fe1dbe68a085cf3ffee167f8aa8
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-04-12 09:55:03 +0300

    Added support for VV (sky obscured)

--- src/main/java/fi/fmi/avi/parser/impl/lexer/token/CloudLayer.java
+++ src/main/java/fi/fmi/avi/parser/impl/lexer/token/CloudLayer.java
@@ -9,6 +9,7 @@ import static fi.fmi.avi.parser.Lexeme.ParsedValueName.VALUE;
 import java.util.regex.Matcher;
 
 import fi.fmi.avi.parser.Lexeme;
+import fi.fmi.avi.parser.Lexeme.ParsedValueName;
 import fi.fmi.avi.parser.ParsingHints;
 import fi.fmi.avi.parser.impl.lexer.RegexMatchingLexemeVisitor;
 
@@ -18,7 +19,7 @@ import fi.fmi.avi.parser.impl.lexer.RegexMatchingLexemeVisitor;
 public class CloudLayer extends RegexMatchingLexemeVisitor {
 
     public enum CloudCover {
-        SKY_CLEAR("SKC"), NO_LOW_CLOUDS("CLR"), NO_SIG_CLOUDS("NSC"), FEW("FEW"), SCATTERED("SCT"), BROKEN("BKN"), OVERCAST("OVC");
+        SKY_CLEAR("SKC"), NO_LOW_CLOUDS("CLR"), NO_SIG_CLOUDS("NSC"), FEW("FEW"), SCATTERED("SCT"), BROKEN("BKN"), OVERCAST("OVC"), SKY_OBSCURED("VV");
 
         private final String code;
 
@@ -56,26 +57,36 @@ public class CloudLayer extends RegexMatchingLexemeVisitor {
         }
 
     }
+    
+    public enum SpecialValue {
+    	AMOUNT_AND_HEIGHT_UNOBSERVABLE_BY_AUTO_SYSTEM;
+    }
 
     public CloudLayer(final Priority prio) {
-        super("^([A-Z]{3})([0-9]{3})(CB|TCU)?$", prio);
+        super("^(([A-Z]{3}|VV)([0-9]{3})(CB|TCU)?)|(/{6})$", prio);
     }
 
     @Override
     public void visitIfMatched(final Lexeme token, final Matcher match, final ParsingHints hints) {
-        CloudCover cloudCover = CloudCover.forCode(match.group(1));
-        int height = Integer.parseInt(match.group(2));
-
-        if (cloudCover != null) {
-            token.identify(Lexeme.Identity.CLOUD);
-            token.setParsedValue(COVER, cloudCover);
+        if (match.group(5) != null) {
+        	//Amount And Height Unobservable By Auto System
+        	token.setParsedValue(ParsedValueName.VALUE, SpecialValue.AMOUNT_AND_HEIGHT_UNOBSERVABLE_BY_AUTO_SYSTEM);
+        	token.identify(CLOUD);
         } else {
-            token.identify(CLOUD, Lexeme.Status.SYNTAX_ERROR, "Unknown cloud cover " + match.group(1));
+	    	CloudCover cloudCover = CloudCover.forCode(match.group(2));
+	        int height = Integer.parseInt(match.group(3));
+	        
+	        if (cloudCover != null) {
+	            token.identify(Lexeme.Identity.CLOUD);
+	            token.setParsedValue(COVER, cloudCover);
+	        } else {
+	            token.identify(CLOUD, Lexeme.Status.SYNTAX_ERROR, "Unknown cloud cover " + match.group(2));
+	        }
+	        if (match.group(4) != null) {
+	            token.setParsedValue(TYPE, CloudType.forCode(match.group(3)));
+	        }
+	        token.setParsedValue(VALUE, height);
+	        token.setParsedValue(UNIT, "hft");
         }
-        if (match.group(3) != null) {
-            token.setParsedValue(TYPE, CloudType.forCode(match.group(3)));
-        }
-        token.setParsedValue(VALUE, height);
-        token.setParsedValue(UNIT, "hft");
     }
 }
