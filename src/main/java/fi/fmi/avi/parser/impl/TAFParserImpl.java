04928281fa6df8ea8b47ad1630b9935963f0278e
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-04-25 16:24:27 +0300

    Added temperature parsing, more generic wind, visibility, weather and cloud parsing method signatures

--- src/main/java/fi/fmi/avi/parser/impl/TAFParserImpl.java
+++ src/main/java/fi/fmi/avi/parser/impl/TAFParserImpl.java
@@ -23,9 +23,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fi.fmi.avi.data.AviationCodeListUser;
+import fi.fmi.avi.data.impl.NumericMeasureImpl;
 import fi.fmi.avi.data.taf.TAF;
+import fi.fmi.avi.data.taf.TAFAirTemperatureForecast;
 import fi.fmi.avi.data.taf.TAFBaseForecast;
 import fi.fmi.avi.data.taf.TAFForecast;
+import fi.fmi.avi.data.taf.impl.TAFAirTemperatureForecastImpl;
 import fi.fmi.avi.data.taf.impl.TAFBaseForecastImpl;
 import fi.fmi.avi.data.taf.impl.TAFImpl;
 import fi.fmi.avi.parser.Lexeme;
@@ -39,7 +42,8 @@ import fi.fmi.avi.parser.ParsingResult;
  */
 public class TAFParserImpl extends AbstractAviMessageParser implements AviMessageSpecificParser<TAF> {
 
-    private static Identity[] zeroOrOneAllowed = { AERODROME_DESIGNATOR, ISSUE_TIME, VALID_TIME, CORRECTION, AMENDMENT, CANCELLATION, NIL };
+    private static Identity[] zeroOrOneAllowed = { AERODROME_DESIGNATOR, ISSUE_TIME, VALID_TIME, CORRECTION, AMENDMENT, CANCELLATION, NIL, MIN_TEMPERATURE,
+            MAX_TEMPERATURE, REMARKS_START };
 
     @Override
     public ParsingResult<TAF> parseMessage(final LexemeSequence lexed, final ParsingHints hints) {
@@ -47,8 +51,8 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
         retval.addIssue(checkZeroOrOne(lexed, zeroOrOneAllowed));
         TAF taf = new TAFImpl();
 
-        Identity[] stopAt = { AERODROME_DESIGNATOR, ISSUE_TIME, NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK,
-                MIN_TEMPERATURE, MAX_TEMPERATURE, WEATHER, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        Identity[] stopAt = { AERODROME_DESIGNATOR, ISSUE_TIME, NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK,
+                MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
 
         findNext(CORRECTION, lexed.getFirstLexeme(), stopAt, (match) -> taf.setStatus(AviationCodeListUser.TAFStatus.CORRECTION));
 
@@ -66,8 +70,8 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
             taf.setStatus(AviationCodeListUser.TAFStatus.NORMAL);
         }
 
-        stopAt = new Identity[] { ISSUE_TIME, NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK, MIN_TEMPERATURE,
-                MAX_TEMPERATURE, WEATHER, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        stopAt = new Identity[] { ISSUE_TIME, NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE,
+                MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
 
         findNext(AERODROME_DESIGNATOR, lexed.getFirstLexeme(), stopAt,
                 (match) -> taf.setAerodromeDesignator(match.getParsedValue(Lexeme.ParsedValueName.VALUE, String.class)), () -> {
@@ -75,7 +79,7 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
                 });
 
         retval.addIssue(updateTAFIssueTime(taf, lexed, hints));
-        stopAt = new Identity[] { VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, WEATHER,
+        stopAt = new Identity[] { VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE,
                 FORECAST_CHANGE_INDICATOR, REMARKS_START };
 
         findNext(NIL, lexed.getFirstLexeme(), stopAt, (match) -> {
@@ -91,7 +95,7 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
 
         retval.addIssue(updateTAFValidTime(taf, lexed, hints));
 
-        stopAt = new Identity[] { SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, WEATHER, FORECAST_CHANGE_INDICATOR,
+        stopAt = new Identity[] { SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR,
                 REMARKS_START };
         findNext(CANCELLATION, lexed.getFirstLexeme(), stopAt, (match) -> {
             taf.setStatus(AviationCodeListUser.TAFStatus.CANCELLATION);
@@ -114,15 +118,15 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
 
     private List<ParsingIssue> updateTAFIssueTime(final TAF fct, final LexemeSequence lexed, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
-        Identity[] before = new Identity[] { NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE,
-                WEATHER, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        Identity[] before = new Identity[] { NIL, VALID_TIME, CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE,
+                MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
         retval.addAll(updateIssueTime(fct, lexed, before, hints));
         return retval;
     }
 
     private List<ParsingIssue> updateTAFValidTime(final TAF fct, final LexemeSequence lexed, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
-        Identity[] before = new Identity[] { CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, WEATHER,
+        Identity[] before = new Identity[] { CANCELLATION, SURFACE_WIND, HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE,
                 FORECAST_CHANGE_INDICATOR, REMARKS_START };
         findNext(VALID_TIME, lexed.getFirstLexeme(), before, (match) -> {
             Integer startDay = match.getParsedValue(Lexeme.ParsedValueName.DAY1, Integer.class);
@@ -132,7 +136,7 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
             if (startDay != null) {
                 fct.setValidityStartDayOfMonth(startDay);
             } else {
-                retval.add(new ParsingIssue(ParsingIssue.Type.SYNTAX_ERROR, "Missing start day of validity"));
+                retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing start day of validity"));
             }
 
             if (endDay != null) {
@@ -144,16 +148,16 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
             if (startHour != null) {
                 fct.setValidityStartHour(startHour);
             } else {
-                retval.add(new ParsingIssue(ParsingIssue.Type.SYNTAX_ERROR, "Missing start hour of validity"));
+                retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing start hour of validity"));
             }
 
             if (endHour != null) {
                 fct.setValidityEndHour(endHour);
             } else {
-                retval.add(new ParsingIssue(ParsingIssue.Type.SYNTAX_ERROR, "Missing end hour of validity"));
+                retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing end hour of validity"));
             }
         }, () -> {
-            retval.add(new ParsingIssue(ParsingIssue.Type.SYNTAX_ERROR, "Missing validity"));
+            retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing validity"));
         });
         return retval;
     }
@@ -161,13 +165,88 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
     private List<ParsingIssue> updateBaseForecast(final TAF fct, final LexemeSequence lexed, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
         TAFBaseForecast baseFct = new TAFBaseForecastImpl();
-        //TODO: temperatures
-        retval.addAll(updateForecastSurfaceWind(baseFct, lexed, hints));
-        retval.addAll(updateVisibility(baseFct, lexed, hints));
-        retval.addAll(updateWeather(baseFct, lexed, hints));
-        retval.addAll(updateClouds(baseFct, lexed, hints));
+
+        Identity[] before = { HORIZONTAL_VISIBILITY, WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        retval.addAll(updateForecastSurfaceWind(baseFct, lexed, before, hints));
+
+        before = new Identity[] { WEATHER, CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        retval.addAll(updateVisibility(baseFct, lexed, before, hints));
+
+        before = new Identity[] { CLOUD, CAVOK, MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        retval.addAll(updateWeather(baseFct, lexed, before, hints));
+
+        before = new Identity[] { MIN_TEMPERATURE, MAX_TEMPERATURE, FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        retval.addAll(updateClouds(baseFct, lexed, before, hints));
+
+        retval.addAll(updateTemperatures(baseFct, lexed, hints));
 
         fct.setBaseForecast(baseFct);
+        return retval;
+    }
+
+    private List<ParsingIssue> updateTemperatures(final TAFBaseForecast baseFct, final LexemeSequence lexed, final ParsingHints hints) {
+        List<ParsingIssue> retval = new ArrayList<>();
+        List<TAFAirTemperatureForecast> temps = new ArrayList<>();
+        TAFAirTemperatureForecast airTemperatureForecast;
+        Identity[] stopAt = { FORECAST_CHANGE_INDICATOR, REMARKS_START };
+        Lexeme minTempToken = findNext(MIN_TEMPERATURE, lexed.getFirstLexeme(), stopAt);
+        Lexeme maxTempToken;
+        while (minTempToken != null) {
+            maxTempToken = findNext(MAX_TEMPERATURE, minTempToken, stopAt);
+            if (maxTempToken != null) {
+                airTemperatureForecast = new TAFAirTemperatureForecastImpl();
+                Integer day = minTempToken.getParsedValue(Lexeme.ParsedValueName.DAY1, Integer.class);
+                Integer hour = minTempToken.getParsedValue(Lexeme.ParsedValueName.HOUR1, Integer.class);
+                Integer value = minTempToken.getParsedValue(Lexeme.ParsedValueName.VALUE, Integer.class);
+
+                if (day != null) {
+                    airTemperatureForecast.setMinTemperatureDayOfMonth(day);
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing day of month for min forecast temperature"));
+                }
+
+                if (hour != null) {
+                    airTemperatureForecast.setMinTemperatureHour(hour);
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing hour of day for min forecast temperature"));
+                }
+
+                if (value != null) {
+                    airTemperatureForecast.setMinTemperature(new NumericMeasureImpl(value, "degC"));
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing value for min forecast temperature"));
+                }
+
+                day = maxTempToken.getParsedValue(Lexeme.ParsedValueName.DAY1, Integer.class);
+                hour = maxTempToken.getParsedValue(Lexeme.ParsedValueName.HOUR1, Integer.class);
+                value = maxTempToken.getParsedValue(Lexeme.ParsedValueName.VALUE, Integer.class);
+
+                if (day != null) {
+                    airTemperatureForecast.setMaxTemperatureDayOfMonth(day);
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing day of month for max forecast temperature"));
+                }
+
+                if (hour != null) {
+                    airTemperatureForecast.setMaxTemperatureHour(hour);
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing hour of day for max forecast temperature"));
+                }
+
+                if (value != null) {
+                    airTemperatureForecast.setMaxTemperature(new NumericMeasureImpl(value, "degC"));
+                } else {
+                    retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Missing value for max forecast temperature"));
+                }
+                temps.add(airTemperatureForecast);
+                minTempToken = findNext(MIN_TEMPERATURE, maxTempToken, stopAt);
+            } else {
+                minTempToken = null;
+            }
+        }
+        if (!temps.isEmpty()) {
+            baseFct.setTemperatures(temps);
+        }
         return retval;
     }
 
@@ -177,22 +256,30 @@ public class TAFParserImpl extends AbstractAviMessageParser implements AviMessag
         return retval;
     }
 
-    private List<ParsingIssue> updateForecastSurfaceWind(final TAFForecast fct, final LexemeSequence lexed, final ParsingHints hints) {
+    private List<ParsingIssue> updateForecastSurfaceWind(final TAFForecast fct, final LexemeSequence lexed, final Identity[] before, final ParsingHints hints) {
+        List<ParsingIssue> retval = new ArrayList<>();
+
+        findNext(SURFACE_WIND, lexed.getFirstLexeme(), before, (match) -> {
+
+        }, () -> {
+            if (fct instanceof TAFBaseForecast) {
+                retval.add(new ParsingIssue(ParsingIssue.Type.MISSING_DATA, "Surface wind is missing from TAF base forecast"));
+            }
+        });
+        return retval;
+    }
+
+    private List<ParsingIssue> updateVisibility(final TAFForecast fct, final LexemeSequence lexed, final Identity[] before, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
         return retval;
     }
 
-    private List<ParsingIssue> updateVisibility(final TAFForecast fct, final LexemeSequence lexed, final ParsingHints hints) {
+    private List<ParsingIssue> updateWeather(final TAFForecast fct, final LexemeSequence lexed, final Identity[] before, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
         return retval;
     }
 
-    private List<ParsingIssue> updateWeather(final TAFForecast fct, final LexemeSequence lexed, final ParsingHints hints) {
-        List<ParsingIssue> retval = new ArrayList<>();
-        return retval;
-    }
-
-    private List<ParsingIssue> updateClouds(final TAFForecast fct, final LexemeSequence lexed, final ParsingHints hints) {
+    private List<ParsingIssue> updateClouds(final TAFForecast fct, final LexemeSequence lexed, final Identity[] before, final ParsingHints hints) {
         List<ParsingIssue> retval = new ArrayList<>();
         return retval;
     }
