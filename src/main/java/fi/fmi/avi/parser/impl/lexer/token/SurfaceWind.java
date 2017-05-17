9b56fd178fd7b04f145f2c74e6d0af2211ab595b
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-05-17 15:18:58 +0300

    Fixed variable surface wind handling for TAF. Added Surface wind TAC tokenizing for Metar

--- src/main/java/fi/fmi/avi/parser/impl/lexer/token/SurfaceWind.java
+++ src/main/java/fi/fmi/avi/parser/impl/lexer/token/SurfaceWind.java
@@ -9,8 +9,13 @@ import static fi.fmi.avi.parser.Lexeme.ParsedValueName.UNIT;
 import java.util.regex.Matcher;
 
 import fi.fmi.avi.data.AviationWeatherMessage;
+import fi.fmi.avi.data.NumericMeasure;
+import fi.fmi.avi.data.metar.Metar;
+import fi.fmi.avi.data.metar.ObservedSurfaceWind;
+import fi.fmi.avi.data.metar.TrendForecast;
 import fi.fmi.avi.data.metar.TrendForecastSurfaceWind;
 import fi.fmi.avi.data.taf.TAFForecast;
+import fi.fmi.avi.data.taf.TAFSurfaceWind;
 import fi.fmi.avi.parser.Lexeme;
 import fi.fmi.avi.parser.ParsingHints;
 import fi.fmi.avi.parser.TokenizingException;
@@ -91,40 +96,65 @@ public class SurfaceWind extends RegexMatchingLexemeVisitor {
 
 		@Override
 		public <T extends AviationWeatherMessage> Lexeme getAsLexeme(T msg, Class<T> clz, final ParsingHints hints, Object... specifier) throws TokenizingException {
-			Lexeme retval = null;
-			TrendForecastSurfaceWind wind = null;
-			TAFForecast fct = getAs(specifier, TAFForecast.class);
-			if (fct != null) {
-				wind = fct.getSurfaceWind();
-			}
-			
-			if (wind != null) {
-				if (!wind.getMeanWindDirection().getUom().equals("deg")) {
-					throw new TokenizingException("Mean wind direction unit is not 'deg': " + wind.getMeanWindDirection().getUom());
-				}
+            Lexeme retval = null;
 
-				StringBuilder builder = new StringBuilder();
+            TAFForecast fct = getAs(specifier, TAFForecast.class);
+            if (fct != null) {
+                TAFSurfaceWind wind = fct.getSurfaceWind();
+                if (wind != null) {
+                    if (!wind.getMeanWindDirection().getUom().equals("deg")) {
+                        throw new TokenizingException("Mean wind direction unit is not 'deg': " + wind.getMeanWindDirection().getUom());
+                    }
 
-				builder.append(String.format("%03d", wind.getMeanWindDirection().getValue().intValue()));
-				int speed = wind.getMeanWindSpeed().getValue().intValue();
-				appendSpeed(builder, speed);
+                    StringBuilder builder = new StringBuilder();
+                    if (wind.isVariableDirection()) {
+                        builder.append("VRB");
+                    }
+                    this.appendCommonWindParameters(builder, wind.getMeanWindSpeed(), wind.getMeanWindDirection(), wind.getWindGust());
+                    retval = this.createLexeme(builder.toString(), Lexeme.Identity.SURFACE_WIND);
 
-				if (wind.getWindGust() != null) {
-					if (!wind.getWindGust().getUom().equals(wind.getMeanWindSpeed().getUom())) {
-						throw new TokenizingException("Wind gust speed unit '" + wind.getWindGust().getUom() + "' is not the same as mean wind speed unit '" + wind.getMeanWindSpeed().getUom() + "'");
-					}
-					builder.append("G");
-					appendSpeed(builder, wind.getWindGust().getValue().intValue());
-				}
+                }
+            } else if (msg instanceof Metar) {
+                TrendForecast trend = getAs(specifier, TrendForecast.class);
+                if (trend != null) {
+                    TrendForecastSurfaceWind wind = trend.getSurfaceWind();
+                    if (wind != null) {
+                        StringBuilder builder = new StringBuilder();
+                        this.appendCommonWindParameters(builder, wind.getMeanWindSpeed(), wind.getMeanWindDirection(), wind.getWindGust());
+                    }
+                } else {
+                    Metar m = (Metar) msg;
+                    ObservedSurfaceWind wind = m.getSurfaceWind();
+                    StringBuilder builder = new StringBuilder();
+                    if (wind.isVariableDirection()) {
+                        builder.append("VRB");
+                    }
+                    this.appendCommonWindParameters(builder, wind.getMeanWindSpeed(), wind.getMeanWindDirection(), wind.getWindGust());
 
-				builder.append(wind.getMeanWindSpeed().getUom().toUpperCase());
-
-                retval = this.createLexeme(builder.toString(), Lexeme.Identity.SURFACE_WIND);
-
+                    //Note: the extreme wind directions token is created by the VariableSurfaceWind.Reconstructor
+                }
             }
 
 			return retval;
 		}
+
+        private void appendCommonWindParameters(StringBuilder builder, NumericMeasure meanSpeed, NumericMeasure meanDirection, NumericMeasure gustSpeed)
+                throws TokenizingException {
+            builder.append(String.format("%03d", meanDirection.getValue().intValue()));
+            int speed = meanSpeed.getValue().intValue();
+            appendSpeed(builder, speed);
+
+            if (gustSpeed != null) {
+                if (!gustSpeed.getUom().equals(gustSpeed.getUom())) {
+                    throw new TokenizingException(
+                            "Wind gust speed unit '" + gustSpeed.getUom() + "' is not the same as mean wind speed unit '" + meanSpeed.getUom() + "'");
+                }
+                builder.append("G");
+                appendSpeed(builder, gustSpeed.getValue().intValue());
+            }
+
+            builder.append(meanSpeed.getUom().toUpperCase());
+        }
 
 		private void appendSpeed(StringBuilder builder, int speed) throws TokenizingException {
 			if (speed < 0 || speed >= 1000) {
