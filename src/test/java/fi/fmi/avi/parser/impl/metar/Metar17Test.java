a984a3ff75718c121b67791b650f536002a375e6
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-01 10:25:55 +0300

    Added METAR 17 POJO test

--- src/test/java/fi/fmi/avi/parser/impl/metar/Metar17Test.java
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar17Test.java
@@ -1,14 +1,18 @@
 package fi.fmi.avi.parser.impl.metar;
 
 import static fi.fmi.avi.parser.Lexeme.Identity.*;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import fi.fmi.avi.data.AviationWeatherMessage;
 import fi.fmi.avi.data.metar.impl.MetarImpl;
 import fi.fmi.avi.parser.ParsingHints;
+import fi.fmi.avi.parser.ParsingIssue;
 import fi.fmi.avi.parser.TokenizingException;
 import fi.fmi.avi.parser.Lexeme.Identity;
+import fi.fmi.avi.parser.ParsingResult.ParsingStatus;
 import fi.fmi.avi.parser.impl.AbstractAviMessageTest;
 
 public class Metar17Test extends AbstractAviMessageTest {
@@ -21,7 +25,7 @@ public class Metar17Test extends AbstractAviMessageTest {
 	@Override
 	public String getMessage() {
 		return
-				"METAR KORD 201004Z 05008KT 1 1/2SM -DZ BR OVC006 03/03 04/54 A2964=";
+				"METAR KORD 201004Z 05008KT 1 1/4SM -DZ BR OVC006 03/03 04/54 A2964=";
 	}
 	
 	@Override
@@ -34,15 +38,23 @@ public class Metar17Test extends AbstractAviMessageTest {
 		return ParsingHints.METAR;
 	}
 	
+	@Override
+	public ParsingStatus getExpectedParsingStatus() {
+		return ParsingStatus.WITH_ERRORS;
+	}
+	
+	@Override
+	public void assertParsingIssues(List<ParsingIssue> parsingIssues) {
+		assertEquals(1, parsingIssues.size());
+		ParsingIssue issue = parsingIssues.get(0);
+		
+		assertEquals(ParsingIssue.Type.SYNTAX_ERROR, issue.getType());
+		assertEquals("More than one of AIR_DEWPOINT_TEMPERATURE in "+getMessage(), issue.getMessage());
+	}
+	
 	// Remove this overridden method once the tokenizer is working
 	@Override
 	public void testTokenizer() throws TokenizingException, IOException {
-		
-	}
-
-	// Remove this overridden method once the parser is working
-	@Override
-	public void testParser() throws IOException {
 		
 	}
 	
