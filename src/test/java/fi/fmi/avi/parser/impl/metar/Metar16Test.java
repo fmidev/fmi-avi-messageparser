325a2dd80a82b2582cb1ef51fa6e51c6aec261b7
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-01 09:56:08 +0300

    Added METAR 16 test POJO

--- src/test/java/fi/fmi/avi/parser/impl/metar/Metar16Test.java
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar16Test.java
@@ -40,12 +40,6 @@ public class Metar16Test extends AbstractAviMessageTest {
 		
 	}
 
-	// Remove this overridden method once the parser is working
-	@Override
-	public void testParser() throws IOException {
-		
-	}
-	
 	@Override
 	public Identity[] getLexerTokenSequenceIdentity() {
 		return new Identity[] {
