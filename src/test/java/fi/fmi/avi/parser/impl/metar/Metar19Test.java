c0b5b6fd3b12f2a1675c83cbbe468a394f761a32
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-16 15:00:01 +0300

    Modified METAR 19 to catch coverage misses. Coverage for different trend forecast time stamp types

--- src/test/java/fi/fmi/avi/parser/impl/metar/Metar19Test.java
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar19Test.java
@@ -3,6 +3,7 @@ package fi.fmi.avi.parser.impl.metar;
 import static fi.fmi.avi.parser.Lexeme.Identity.AERODROME_DESIGNATOR;
 import static fi.fmi.avi.parser.Lexeme.Identity.AIR_DEWPOINT_TEMPERATURE;
 import static fi.fmi.avi.parser.Lexeme.Identity.AIR_PRESSURE_QNH;
+import static fi.fmi.avi.parser.Lexeme.Identity.CAVOK;
 import static fi.fmi.avi.parser.Lexeme.Identity.CLOUD;
 import static fi.fmi.avi.parser.Lexeme.Identity.END_TOKEN;
 import static fi.fmi.avi.parser.Lexeme.Identity.FORECAST_CHANGE_INDICATOR;
@@ -31,7 +32,7 @@ public class Metar19Test extends AbstractAviMessageTest<String, Metar> {
 	@Override
 	public String getMessage() {
 		return
-				"METAR EFHK 111111Z 15008G20KT 0700 R04R/M1500VP2000N R15/P1000VM3000U R22L/1200N R04L/1000VP1500U SN VV006 M08/M10 Q1023 " + "WM01/H14 99//9999 NOSIG=";
+				"METAR EFHK 111111Z 15008G20KT 0700 R04R/M1500VP2000N R15/P1000VM3000U R22L/1200N R04L/1000VP1500U SN VV006 M08/M10 Q1023 " + "WM01/H14 99//9999 BECMG AT0927 CAVOK TEMPO FM1051=";
 	}
 	
 	@Override
@@ -44,7 +45,7 @@ public class Metar19Test extends AbstractAviMessageTest<String, Metar> {
 		return new Identity[] {
 				METAR_START, AERODROME_DESIGNATOR, ISSUE_TIME, SURFACE_WIND, HORIZONTAL_VISIBILITY, RUNWAY_VISUAL_RANGE,
                 RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, WEATHER, CLOUD, AIR_DEWPOINT_TEMPERATURE, AIR_PRESSURE_QNH,
-                SEA_STATE, RUNWAY_STATE, FORECAST_CHANGE_INDICATOR, END_TOKEN
+                SEA_STATE, RUNWAY_STATE, FORECAST_CHANGE_INDICATOR, FORECAST_CHANGE_INDICATOR, CAVOK, FORECAST_CHANGE_INDICATOR, FORECAST_CHANGE_INDICATOR, END_TOKEN
 		};
 	}
 
