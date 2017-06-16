252713d1bdc7a342313a79aa0dfa5fc4d500690b
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-16 13:44:01 +0300

    Added METAR 19 to catch coverage misses. First up: surface wind gusts

---
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar19Test.java
@@ -0,0 +1,60 @@
+package fi.fmi.avi.parser.impl.metar;
+
+import static fi.fmi.avi.parser.Lexeme.Identity.AERODROME_DESIGNATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_DEWPOINT_TEMPERATURE;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_PRESSURE_QNH;
+import static fi.fmi.avi.parser.Lexeme.Identity.CLOUD;
+import static fi.fmi.avi.parser.Lexeme.Identity.END_TOKEN;
+import static fi.fmi.avi.parser.Lexeme.Identity.FORECAST_CHANGE_INDICATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.HORIZONTAL_VISIBILITY;
+import static fi.fmi.avi.parser.Lexeme.Identity.ISSUE_TIME;
+import static fi.fmi.avi.parser.Lexeme.Identity.METAR_START;
+import static fi.fmi.avi.parser.Lexeme.Identity.RUNWAY_VISUAL_RANGE;
+import static fi.fmi.avi.parser.Lexeme.Identity.SEA_STATE;
+import static fi.fmi.avi.parser.Lexeme.Identity.SURFACE_WIND;
+import static fi.fmi.avi.parser.Lexeme.Identity.WEATHER;
+
+import fi.fmi.avi.data.metar.Metar;
+import fi.fmi.avi.data.metar.impl.MetarImpl;
+import fi.fmi.avi.parser.ConversionSpecification;
+import fi.fmi.avi.parser.Lexeme.Identity;
+import fi.fmi.avi.parser.impl.AbstractAviMessageTest;
+
+public class Metar19Test extends AbstractAviMessageTest<String, Metar> {
+
+	@Override
+	public String getJsonFilename() {
+		return "metar/metar19.json";
+	}
+	
+	@Override
+	public String getMessage() {
+		return
+				"METAR EFHK 111111Z 15008G20KT 0700 R04R/1500N R15/1000U R22L/1200N R04L/1000VP1500U SN VV006 M08/M10 Q1023 " + "WM01/S1 TEMPO TL1530 +SHRA BKN012CB=";
+	}
+	
+	@Override
+	public String getTokenizedMessagePrefix() {
+		return "";
+	}
+	
+	@Override
+	public Identity[] getLexerTokenSequenceIdentity() {
+		return new Identity[] {
+				METAR_START, AERODROME_DESIGNATOR, ISSUE_TIME, SURFACE_WIND, HORIZONTAL_VISIBILITY, RUNWAY_VISUAL_RANGE,
+                RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, WEATHER, CLOUD, AIR_DEWPOINT_TEMPERATURE, AIR_PRESSURE_QNH,
+                SEA_STATE, FORECAST_CHANGE_INDICATOR, FORECAST_CHANGE_INDICATOR, WEATHER, CLOUD, END_TOKEN
+		};
+	}
+
+	@Override
+    public ConversionSpecification<String, Metar> getParserSpecification() {
+        return ConversionSpecification.TAC_TO_METAR;
+    }
+
+    @Override
+    public Class<? extends Metar> getTokenizerImplmentationClass() {
+        return MetarImpl.class;
+    }
+
+}
