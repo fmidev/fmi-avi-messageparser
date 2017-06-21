90791ce5caeb3fa0aa94f88992a798886f58e1d2
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-21 14:18:22 +0300

    Add METAR test case where RVR is in feet

---
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar25Test.java
@@ -0,0 +1,63 @@
+package fi.fmi.avi.parser.impl.metar;
+
+import static fi.fmi.avi.parser.Lexeme.Identity.AERODROME_DESIGNATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_DEWPOINT_TEMPERATURE;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_PRESSURE_QNH;
+import static fi.fmi.avi.parser.Lexeme.Identity.CLOUD;
+import static fi.fmi.avi.parser.Lexeme.Identity.END_TOKEN;
+import static fi.fmi.avi.parser.Lexeme.Identity.FORECAST_CHANGE_INDICATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.HORIZONTAL_VISIBILITY;
+import static fi.fmi.avi.parser.Lexeme.Identity.ISSUE_TIME;
+import static fi.fmi.avi.parser.Lexeme.Identity.METAR_START;
+import static fi.fmi.avi.parser.Lexeme.Identity.RECENT_WEATHER;
+import static fi.fmi.avi.parser.Lexeme.Identity.RUNWAY_VISUAL_RANGE;
+import static fi.fmi.avi.parser.Lexeme.Identity.SURFACE_WIND;
+import static fi.fmi.avi.parser.Lexeme.Identity.WEATHER;
+import static fi.fmi.avi.parser.Lexeme.Identity.WIND_SHEAR;
+
+import fi.fmi.avi.data.metar.Metar;
+import fi.fmi.avi.data.metar.impl.MetarImpl;
+import fi.fmi.avi.parser.ConversionSpecification;
+import fi.fmi.avi.parser.Lexeme.Identity;
+import fi.fmi.avi.parser.impl.AbstractAviMessageTest;
+
+public class Metar25Test extends AbstractAviMessageTest<String, Metar> {
+
+	@Override
+	public String getJsonFilename() {
+		return "metar/metar25.json";
+	}
+	
+	// Almost exactly the same as Metar 18 except runway visual range is given in feet 
+	@Override
+	public String getMessage() {
+		return
+				"METAR EFHK 111111Z 15008KT 0700 R04R/1500NFT R15/1000U R22L/1200N R04L/1000VP1500U SN VV006 M08/M10 Q1023 RESN" + " WS R04R TEMPO 0900=";
+	}
+	
+	@Override
+	public String getTokenizedMessagePrefix() {
+		return "";
+	}
+	
+	@Override
+	public Identity[] getLexerTokenSequenceIdentity() {
+		return new Identity[] {
+				METAR_START, AERODROME_DESIGNATOR, ISSUE_TIME, SURFACE_WIND, HORIZONTAL_VISIBILITY, RUNWAY_VISUAL_RANGE,
+                RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, WEATHER, CLOUD, AIR_DEWPOINT_TEMPERATURE, AIR_PRESSURE_QNH,
+                RECENT_WEATHER, WIND_SHEAR, FORECAST_CHANGE_INDICATOR, HORIZONTAL_VISIBILITY, END_TOKEN
+		};
+	}
+
+	
+	@Override
+    public ConversionSpecification<String, Metar> getParserSpecification() {
+        return ConversionSpecification.TAC_TO_METAR;
+    }
+
+    @Override
+    public Class<? extends Metar> getTokenizerImplmentationClass() {
+        return MetarImpl.class;
+    }
+
+}
