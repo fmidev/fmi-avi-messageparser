5ae6a8c174b56f3286e1dfaedbb689000101bd0e
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-21 11:50:33 +0300

    Add METAR runwaystate test cases

---
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar22Test.java
@@ -0,0 +1,58 @@
+package fi.fmi.avi.parser.impl.metar;
+
+import static fi.fmi.avi.parser.Lexeme.Identity.AERODROME_DESIGNATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_DEWPOINT_TEMPERATURE;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_PRESSURE_QNH;
+import static fi.fmi.avi.parser.Lexeme.Identity.CAVOK;
+import static fi.fmi.avi.parser.Lexeme.Identity.END_TOKEN;
+import static fi.fmi.avi.parser.Lexeme.Identity.ISSUE_TIME;
+import static fi.fmi.avi.parser.Lexeme.Identity.METAR_START;
+import static fi.fmi.avi.parser.Lexeme.Identity.RUNWAY_STATE;
+import static fi.fmi.avi.parser.Lexeme.Identity.SURFACE_WIND;
+
+import fi.fmi.avi.data.metar.Metar;
+import fi.fmi.avi.data.metar.impl.MetarImpl;
+import fi.fmi.avi.parser.ConversionSpecification;
+import fi.fmi.avi.parser.Lexeme.Identity;
+import fi.fmi.avi.parser.impl.AbstractAviMessageTest;
+
+public class Metar22Test extends AbstractAviMessageTest<String, Metar> {
+
+	@Override
+	public String getJsonFilename() {
+		return "metar/metar22.json";
+	}
+	
+	@Override
+	public String getMessage() {
+		return
+				"METAR EFTU 011350Z VRB02KT CAVOK " +
+				"22/12 Q1008 " +
+				"R15L/410038 R64R/418338=";
+	}
+	
+	@Override
+	public String getTokenizedMessagePrefix() {
+		return "";
+	}
+
+	@Override
+	public Identity[] getLexerTokenSequenceIdentity() {
+		return new Identity[] {
+				METAR_START, AERODROME_DESIGNATOR, ISSUE_TIME, SURFACE_WIND, CAVOK,
+				AIR_DEWPOINT_TEMPERATURE, AIR_PRESSURE_QNH, 
+				RUNWAY_STATE, RUNWAY_STATE, END_TOKEN
+		};
+	}
+
+    @Override
+    public ConversionSpecification<String, Metar> getParserSpecification() {
+        return ConversionSpecification.TAC_TO_METAR;
+    }
+
+    @Override
+    public Class<? extends Metar> getTokenizerImplmentationClass() {
+        return MetarImpl.class;
+    }
+
+}
