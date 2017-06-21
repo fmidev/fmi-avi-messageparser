d768c939aa71f3636c11064450e84e6402641603
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-06-21 13:06:45 +0300

    Add METAR test with a minimum visibility in SW instead of N

---
+++ src/test/java/fi/fmi/avi/parser/impl/metar/Metar23Test.java
@@ -0,0 +1,63 @@
+package fi.fmi.avi.parser.impl.metar;
+
+import static fi.fmi.avi.parser.Lexeme.Identity.AERODROME_DESIGNATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_DEWPOINT_TEMPERATURE;
+import static fi.fmi.avi.parser.Lexeme.Identity.AIR_PRESSURE_QNH;
+import static fi.fmi.avi.parser.Lexeme.Identity.CLOUD;
+import static fi.fmi.avi.parser.Lexeme.Identity.CORRECTION;
+import static fi.fmi.avi.parser.Lexeme.Identity.END_TOKEN;
+import static fi.fmi.avi.parser.Lexeme.Identity.FORECAST_CHANGE_INDICATOR;
+import static fi.fmi.avi.parser.Lexeme.Identity.HORIZONTAL_VISIBILITY;
+import static fi.fmi.avi.parser.Lexeme.Identity.ISSUE_TIME;
+import static fi.fmi.avi.parser.Lexeme.Identity.METAR_START;
+import static fi.fmi.avi.parser.Lexeme.Identity.RECENT_WEATHER;
+import static fi.fmi.avi.parser.Lexeme.Identity.RUNWAY_VISUAL_RANGE;
+import static fi.fmi.avi.parser.Lexeme.Identity.SURFACE_WIND;
+import static fi.fmi.avi.parser.Lexeme.Identity.VARIABLE_WIND_DIRECTION;
+import static fi.fmi.avi.parser.Lexeme.Identity.WEATHER;
+
+import fi.fmi.avi.data.metar.Metar;
+import fi.fmi.avi.data.metar.impl.MetarImpl;
+import fi.fmi.avi.parser.ConversionSpecification;
+import fi.fmi.avi.parser.Lexeme.Identity;
+import fi.fmi.avi.parser.impl.AbstractAviMessageTest;
+
+public class Metar23Test extends AbstractAviMessageTest<String, Metar> {
+
+	@Override
+	public String getJsonFilename() {
+		return "metar/metar23.json";
+	}
+	
+	@Override
+	public String getMessage() {
+		return
+				"METAR COR EFUT 111115Z 18004KT 150V240 1500 0500SW R04R/1500N R15/M0050D R22L/1200N R04L/P1000U SN VV006 M08/M10 " + "Q1023 RESN TEMPO 0900=";
+	}
+	
+	@Override
+	public String getTokenizedMessagePrefix() {
+		return "";
+	}
+	
+	@Override
+	public Identity[] getLexerTokenSequenceIdentity() {
+		return new Identity[] {
+				METAR_START, CORRECTION, AERODROME_DESIGNATOR, ISSUE_TIME, SURFACE_WIND, VARIABLE_WIND_DIRECTION,
+                HORIZONTAL_VISIBILITY, HORIZONTAL_VISIBILITY, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, RUNWAY_VISUAL_RANGE, WEATHER,
+                CLOUD, AIR_DEWPOINT_TEMPERATURE, AIR_PRESSURE_QNH, RECENT_WEATHER, FORECAST_CHANGE_INDICATOR,
+                HORIZONTAL_VISIBILITY, END_TOKEN
+		};
+	}
+
+	@Override
+    public ConversionSpecification<String, Metar> getParserSpecification() {
+        return ConversionSpecification.TAC_TO_METAR;
+    }
+
+    @Override
+    public Class<? extends Metar> getTokenizerImplmentationClass() {
+        return MetarImpl.class;
+    }
+
+}
